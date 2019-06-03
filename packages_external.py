d053dff91c34302e51953f0450ba1f36d15cafa9
Shaun Koon <shaun.koon@hotmail.com>
2019-06-03 12:27:59 +0100

    finished packages

--- packages_external.py
+++ packages_external.py
@@ -1,3 +1,11 @@
+# External packages need to be installed
+# You can find packages on http://pypi.org/search/?q=faker
+# Lets look at the package of 'requests'
+# install using py
+
+import requests
+
+sparta_home _page = https://www.spartaglobal.com/
 
 
 
